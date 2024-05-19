a389cf9bfefb1af2d15d57ac1afeb8cd2aaa2fb3
hugeletters <ckuka71@gmail.com>
2024-05-19 15:27:06 +0200

    add dialyxir for static analysis

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Rocketchat.MixProject do
     [
       {:phoenix, "~> 1.7.12"},
       {:phoenix_ecto, "~> 4.4"},
+      {:dialyxir, "~> 1.4", only: :dev},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.0"},
