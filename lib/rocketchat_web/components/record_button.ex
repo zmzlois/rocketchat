3a663c415a87f787815db1538f97439c5898b5a7
hugeletters <ckuka71@gmail.com>
2024-05-29 17:22:51 +0200

    reset on invalid file uploads with a flash message

--- lib/rocketchat_web/components/record_button.ex
+++ lib/rocketchat_web/components/record_button.ex
@@ -1,5 +1,6 @@
 defmodule RocketchatWeb.RecordButton do
   use RocketchatWeb, :live_component
+  alias Phoenix.LiveView
   # #todo - how to avoid yucky settimeout for submission
   ## assign_uploads has progress callback - save recording there instead of submit
   ## replace button type with submit when starting the recording?
@@ -23,7 +24,7 @@ defmodule RocketchatWeb.RecordButton do
   end
 
   def handle_event("recorded", _params, socket) do
-    {:noreply, socket |> assign(recording?: false)}
+    {:noreply, socket |> flush_invalid_uploads() |> assign(recording?: false)}
   end
 
   def handle_event("uploaded", _params, socket) do
@@ -45,6 +46,7 @@ defmodule RocketchatWeb.RecordButton do
   def render(assigns) do
     ~H"""
     <form phx-submit="uploaded" phx-target={@myself} class="flex flex-col items-center p-2">
+      <.flash_group flash={@flash} />
       <button
         id="audio-recorder"
         type="button"
@@ -61,12 +63,28 @@ defmodule RocketchatWeb.RecordButton do
       </button>
       <%!-- doesn't work w/o phx-change ¯\_(ツ)_/¯ --%>
       <.live_file_input upload={@uploads.audio} phx-change="recorded" class="sr-only" />
-      <%= for entry <- @uploads.audio.entries do %>
-        <%= for error <- upload_errors(@uploads.audio, entry) do %>
-          <%= error %>
-        <% end %>
-      <% end %>
     </form>
     """
+  end
+
+  defp flush_invalid_uploads(socket = %LiveView.Socket{assigns: %{uploads: uploads}}) do
+    alias LiveView.UploadEntry
+
+    uploads.audio.entries
+    |> Stream.filter(fn entry = %UploadEntry{} -> !entry.valid? end)
+    |> Enum.reduce(socket, fn entry = %UploadEntry{}, socket ->
+      error = upload_errors(uploads.audio, entry) |> List.first()
+
+      cancel_upload(socket, :audio, entry.ref)
+      |> put_flash(:error, error_message(error))
+    end)
+  end
+
+  defp error_message(:too_large) do
+    "Recorded message exceeds our file limit"
+  end
+
+  defp error_message(_) do
+    "Unexpected error has occured"
   end
 end
