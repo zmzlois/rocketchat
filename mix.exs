0449afda8d7507ee058f4ff5b0c66cd1351ece31
Ilia <eilia.abedian@yahoo.com>
2024-06-01 16:55:31 -0700

    add: install req library for HTTP requests

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule Rocketchat.MixProject do
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0.0-rc.0", override: true},
+      {:req, "~> 0.5.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
